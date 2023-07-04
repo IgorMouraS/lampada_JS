8be8e96d5f0f3d742f5d546721a1854bdced5b7c
Igor Moura Sousa <90483859+IgorMouraS@users.noreply.github.com>
2023-07-04 15:47:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const lampadaOn = document.querySelector(".lampadaOn");
 const lampadaOff = document.querySelector(".lampadaOff");
+const tituloEl = document.querySelector(".titulo");
 
 lampadaOff.addEventListener("click", (e) => {
     e.preventDefault();
+
+    tituloEl.innerText = "Parabéns!";
+
     lampadaOff.style.display = "none";
     lampadaOn.style.display = "block";
+
 });
 lampadaOn.addEventListener("click", (e) => {
     e.preventDefault();
+
+    tituloEl.innerHTML = "Acenda a lampada!";
+
     lampadaOn.style.display = "none";
     lampadaOff.style.display = "block";
 });
